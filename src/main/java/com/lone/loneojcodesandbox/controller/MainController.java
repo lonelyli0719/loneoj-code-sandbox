0a75504e502c6875fdd83a08af1f2ee60c14fd5c
lonelyli0719 <794156416@qq.com>
2025-08-09 21:49:00 +0800

    loneoj代码沙箱暴漏api

--- src/main/java/com/lone/loneojcodesandbox/controller/MainController.java
+++ src/main/java/com/lone/loneojcodesandbox/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.lone.loneojcodesandbox.controller;
 
+import com.lone.loneojcodesandbox.JavaNativeCodeSandbox;
+import com.lone.loneojcodesandbox.model.ExecuteCodeRequest;
+import com.lone.loneojcodesandbox.model.ExecuteCodeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +15,38 @@ import javax.servlet.http.HttpServletResponse;
 @RestController("/")
 public class MainController {
 
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
+    @Resource
+    private JavaNativeCodeSandbox javaNativeCodeSandbox;
+
     @GetMapping("/health")
     public String healthCheck() {
         return "ok";
+    }
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    @PostMapping("/executeCode")
+    ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                    HttpServletResponse response) {
+        // 基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
+        if (executeCodeRequest == null) {
+            throw new RuntimeException("请求参数为空");
+        }
+        return javaNativeCodeSandbox.executeCode(executeCodeRequest);
     }
 
 }
